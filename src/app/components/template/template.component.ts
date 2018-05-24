3f08819d9261d6e9f4932c349db624aa4c3ee475
nekrapula99 <nekrapula@gmail.com>
2018-05-23 23:13:29 -0500

    segundo commit Forms

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styles: []
 })
-export class TemplateComponent implements OnInit {
+export class TemplateComponent  {
 
+
+  usuario:Object = {
+    nombre: "Nekrapula",
+    apellido: "Ob",
+    correo:""
+  }
   constructor() { }
 
-  ngOnInit() {
+
+  guardar(forma:NgForm){
+    console.log("Form Guardado!!");
+    console.log("forma: ",forma);
+    console.log("Valor: ",forma.value);
+
+    console.log("Usuario: ",this.usuario);
+
+    console.log("status: ",forma.status);
+    console.log("touched: ",forma.touched);
   }
 
 }
